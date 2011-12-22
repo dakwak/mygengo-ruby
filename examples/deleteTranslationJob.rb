7c923f6878850f3389f64e2be702f3c054aca0f5
Ryan McGrath <ryan@venodesigns.net>
2011-12-22 01:32:34 -0500

    Remove sandbox keys...

--- examples/deleteTranslationJob.rb
+++ examples/deleteTranslationJob.rb
@@ -5,8 +5,8 @@ require 'mygengo'
 # print out response JSON.
 
 mygengo = MyGengo::API.new({
-    :public_key => 'Kqw3@uQ=UYejce5U3u-P6s6-A}i#UELr8-g4oaA~eRSfjFu{HS@j@{4=]}rmoaOR',
-    :private_key => 'WHOWYPoPn7oCoZ@wn~76V=DWLOue{8{pkNS$ym#[e3t2qY|SEN7erfJS3cqcdngv',
+    :public_key => '',
+    :private_key => '',
     :sandbox => true
 })
 
--- examples/getAccountBalance.rb
+++ examples/getAccountBalance.rb
@@ -2,16 +2,10 @@
 require 'mygengo'
 
 mygengo = MyGengo::API.new({
-	:public_key => 'x4n7^jxvy)Pgp17hX5p[2x76A(0Yhk{76Lz2)bJ]8s5X)ShtW2hXyRZ^(#9}SKLh',
-	:private_key => 'JimAgf8tkM^I}_|{jue5umYeqN$HHmL0LtXpZt8)aGf7itvzF2^zDtHgR_0MG754',
+	:public_key => '',
+	:private_key => '',
 	:sandbox => true,
 })
-
-#mygengo = MyGengo::API.new({
-#	:public_key => 'x4n7^jxvy)Pgp17hX5p[2x76A(0Yhk{76Lz2)bJ]8s5X)ShtW2hXyRZ^(#9}SKLh',
-#	:private_key => '',
-#	:sandbox => true,
-#})
 
 # Return the number of credits left on your account.
 puts mygengo.getAccountBalance()
